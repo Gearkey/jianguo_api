7452595079f18a9ca8da40f1ed02b245636ae9d9
Gearkey <gearkey@163.com>
2021-08-09 19:20:36 +0800

    实现移动文件

--- jianguo_api.py
+++ jianguo_api.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import urllib.request
 from datetime import datetime
 
@@ -23,14 +24,25 @@ class jianguo_api(object):
         self._cookies = None
         self._headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
             "Referer": "https://www.jianguoyun.com/",
+            "TE": "Trailers",
         }
     
     def _get(self, url):
         request = urllib.request.Request(url=url, headers=self._headers)
         response = urllib.request.urlopen(request)
 
-        return response.read().decode('utf-8')
+        return response.read().decode("utf-8")
+    
+    def _post(self, url, data):
+        datas = urllib.parse.urlencode(data).encode("utf-8")
+        
+        request = urllib.request.Request(url=url, headers=self._headers, data=datas)
+        response = urllib.request.urlopen(request)
+
+        return response.read().decode("utf-8")
 
     # 设置单文件大小限制（网页版限制500M）
     def set_max_size(self, max_size=500) -> int:
@@ -46,6 +58,7 @@ class jianguo_api(object):
     # 获取 snd_magic
     def get_snd_magic(self) -> str:
         snd_magic = ""
+
         return snd_magic
     
     # 获取用户 Cookie
@@ -56,7 +69,8 @@ class jianguo_api(object):
     def login_by_cookie(self, cookie: dict) -> int:
         self._cookies = cookie
         self._headers['cookie'] = self._cookies
-        self.get_user_info()
+        self._uesr_info = json.loads(self.get_user_info())
+
         return jianguo_api.SUCCESS
 
     # 注销
@@ -84,23 +98,30 @@ class jianguo_api(object):
         pass
 
     # 获取文件列表
-    def get_file_list(self, snd_id, path, folder_id=-1) -> list:
-        file_list = []
-        resp = self._get(self._host_url + "/d/ajax/browse" + path + "?sndId=" + snd_id + "&sndMagic=" + self.get_snd_magic(snd_id) +"&_=" + datetime.time())
-
-        return file_list
-
-    # 获取子文件夹列表
-    def get_dir_list(self, folder_id=-1) -> list:
-        pass
+    def get_file_list(self, snd_id, snd_magic, path) -> dict:
+        file_list = self._get(self._host_url + "/d/ajax/browse" + path + "?sndId=" + snd_id + "&sndMagic=" + snd_magic)
+        return json.loads(file_list)
 
     # 移动文件
-    def move_file(self, file_id, folder_id=-1) -> int:
-        pass
+    def move_file(self, snd_id, snd_magic, src_path, dst_dir, dst_snd_id="", dst_snd_magic="") -> int:
+        data = {
+            "srcSndId": snd_id,
+            "srcSndMagic": snd_magic,
+            "srcPath": src_path,
+            "dstDir": dst_dir,
+        }
 
-    # 移动文件夹
-    def move_folder(self, folder_id, parent_folder_id=-1) -> int:
-        pass
+        ## 如果没填目标根文件夹的 snd_id 和 snd_magic，即在本根目录内移动
+        if dst_snd_id == "":
+            dst_snd_id = snd_id
+            dst_snd_magic = snd_magic
+        
+        ## 移动后返回一个操作的 uuid，通过 uuid 判断操作是否成功
+        uuid = self._post(self._host_url + "/d/ajax/submitMove?sndId=" + dst_snd_id + "&sndMagic=" + dst_snd_magic, data)
+        resp = self._get(self._host_url + "/d/ajax/moveProgress?uuid=" + json.loads(uuid)["uuid"])
+
+        if json.loads(resp)["state"] == "SUCCESS": return jianguo_api.SUCCESS
+        else: return jianguo_api.FAILED
 
     # 重命名文件
     def rename_file(self, folder_id, folder_name) -> int:
